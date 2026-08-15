6692c556c3353e2f543ab188b9ff4172243420fa
matthewdsha <matthew.du04@gmail.com>
2026-08-15 01:37:51 -0500

    Added testcontainers and workflows for Inventory Service and Notification Service.

--- inventory-service/src/test/java/com/sentinel/inventory_service/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/sentinel/inventory_service/InventoryServiceApplicationTests.java
@@ -2,9 +2,33 @@ package com.sentinel.inventory_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 
 @SpringBootTest
+@Testcontainers
 class InventoryServiceApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+			.withDatabaseName("inventory")
+			.withUsername("test_user")
+			.withPassword("test_password");
+
+	@Container
+	static ConfluentKafkaContainer kafka = new ConfluentKafkaContainer("confluentinc/cp-kafka::7.7.1");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+		registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+	}
 
 	@Test
 	void contextLoads() {
--- notification-service/src/test/java/com/sentinel/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/sentinel/notification_service/NotificationServiceApplicationTests.java
@@ -1,10 +1,30 @@
 package com.sentinel.notification_service;
 
+import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 
 @SpringBootTest
+@Testcontainers
 class NotificationServiceApplicationTests {
+
+	@Container
+	static RedisContainer redis = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("7"));
+
+	@Container
+	static ConfluentKafkaContainer kafka = new ConfluentKafkaContainer("confluentinc/cp-kafka:7.7.1");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", redis::getHost);
+		registry.add("spring.data.redis.host", redis::getFirstMappedPort);
+		registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+	}
 
 	@Test
 	void contextLoads() {
